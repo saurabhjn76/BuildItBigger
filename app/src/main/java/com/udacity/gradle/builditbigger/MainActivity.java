93b7139ba383e1c2b6cd228b5d172f99c9ae89d6
saurabhjn76 <saurabhjn76@gmail.com>
2017-01-24 00:27:47 +0530

    Finished Step-2(Adding of android Library)

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.JokeLibrary;
+
+import saurabhjn76.com.myjokelibrary.JokeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -45,7 +48,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, jokeLibrary.getRandomJoke(), Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this, jokeLibrary.getRandomJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeActivity.class);
+        intent.putExtra(JokeActivity.JOKEINTENT,jokeLibrary.getRandomJoke());
+        startActivity(intent);
+
     }
 
 
--- myjokelibrary/src/main/java/saurabhjn76/com/myjokelibrary/JokeActivity.java
+++ myjokelibrary/src/main/java/saurabhjn76/com/myjokelibrary/JokeActivity.java
@@ -2,12 +2,16 @@ package saurabhjn76.com.myjokelibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
-
+    public final static String JOKEINTENT="JOKEINTENT";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
+        TextView textView =(TextView) findViewById(R.id.jokeview);
+      //  if(getIntent().getStringArrayExtra(JOKEINTENT)!=null)
+        textView.setText(getIntent().getStringExtra(JOKEINTENT));
     }
 }
